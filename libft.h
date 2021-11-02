0ab8d277f7e49821b0b14809c9ca7fdf1ab02eb6
Moussa Bistami <mbistami@e1r6p2.1337.ma>
2021-11-02 10:30:41 +0100

    isprint normed

--- libft.h
+++ libft.h
@@ -2,8 +2,10 @@
 #include "ft_isalpha.c"
 #include "ft_isdigit.c"
 #include "ft_isalnum.c"
+#include "ft_isprint.c"
 
 int ft_isdigit(int v);
 int ft_isupper(int v);
 int ft_isalpha(int v);
 int ft_isalnum(int v);
+int ft_isprint(int v);
--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 
 int main(void){
-	printf("%d|", ft_isupper('z'));
-	printf("%d|", ft_isdigit('5'));
-	printf("%d|", ft_isalpha('+'));
-	printf("%d|", ft_isalnum('2'));
+	printf("%d|", ft_isupper('z')); // returns positive if uppercase.
+	printf("%d|", ft_isdigit('5')); // returns positive if digit.
+	printf("%d|", ft_isalpha('+'));	// returns positive if alphanumeric.
+	printf("%d|", ft_isalnum('2')); // returns positive if number.
+	for(int c = 1; c <= 127; ++c) // testing isprint()
+   		if (ft_isprint(c)!= 0)
+             printf("%c ", c);
 }
