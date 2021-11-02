059a5283080b0f725a71d310cccabb00e46e6939
Moussa Bistami <mbistami@e1r6p2.1337.ma>
2021-11-02 09:22:37 +0100

    intial

---
+++ libft.h
@@ -0,0 +1,7 @@
+#include "ft_isupper.c"
+#include "ft_isalpha.c"
+#include "ft_isdigit.c"
+
+int ft_isdigit(int v);
+int ft_isupper(int v);
+int ft_isalpha(int v);
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void){
+	printf("%d|", ft_isupper('z'));
+	printf("%d|", ft_isdigit('5'));
+	printf("%d|", ft_isalpha('+'));
+}
