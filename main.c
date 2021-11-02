592f6d0264832fa2a3d42fed93fc78a027a1e8be
Moussa Bistami <mbistami@e1r7p4.1337.ma>
2021-11-02 17:31:50 +0100

    ft_memcpy

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "libft.h"
 
+char string[10] = "33tas";
+char stringDest[10] = "data1";
+
 int main(void){
 	// printf("%d|", ft_isupper('z')); // returns positive if uppercase.
 	// printf("%d|", ft_isdigit('5')); // returns positive if digit.
@@ -9,15 +12,11 @@ int main(void){
    	// 	if (ft_isprint(c)!= 0)
     //          printf("%c ", c);
 	// printf("%d", ft_strlen("ssss\0ssss"));
-	char string[10] = "33tas";
 	// printf("%s",string);
 	//char *p = memset(string, 'a', 2);
 	// char *p1 = ft_memset(string, 'a', 2);
 	// printf("\n%s | %s", p, p1);
-
 	//printf("%s", ft_bzero(string, 5));
-
-	char stringDest[10] = "data1";
 
 	printf("%s | %s | %s", ft_memcpy(string, stringDest, 2), stringDest, string);
 }
