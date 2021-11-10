6a6b61e0856484b9304a9dfe4bf6e03a79126820
gruz <mbistami@e2r12p11.1337.ma>
2021-11-10 22:28:03 +0100

    fix ft_strrchr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbistami <mbistami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 07:08:52 by mbistami          #+#    #+#             */
-/*   Updated: 2021/11/10 19:27:08 by mbistami         ###   ########.fr       */
+/*   Updated: 2021/11/10 22:27:08 by mbistami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,7 @@ char			g_overlap_test[] = "hello world";
 
 int	main(void)
 {
-	printf("%s", ft_strchr("there is so \0ma\0ny \0 \\0 in t\0his stri\0ng !\0\0\0\0", '\0'));
+	char     str[] = "sthere\0ma\0ny \0 \\0 in t\0his stri\0ng !\0\0\0sx";
+	//printf("\n%c\n", *(*(&str + 1) - 2));
+	printf("%s", ft_strrchr("abbbbbbbb", 'a'));
 }
--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gruz <gruz@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mbistami <mbistami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 20:17:32 by mbistami          #+#    #+#             */
-/*   Updated: 2021/11/05 23:13:31 by gruz             ###   ########.fr       */
+/*   Updated: 2021/11/10 22:27:42 by mbistami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 char	*ft_strrchr(const char *string, int searchChar)
 {
 	char	*s;
-	int		len;
-	char	*last;
+	size_t	i;
+	char	k;
 
 	s = (char *)string;
-	len = ft_strlen(s);
-	s = s + len - 1;
-	while (*s--)
-		if (*s == searchChar)
-			return ((char *) string);
+	i = ft_strlen(string);
+	while (i != -1)
+	{
+		if (s[i] == searchChar)
+			return (&s[i]);
+		i--;
+	}
+	if (s[i] == '\0' && searchChar == '\0')
+		return (&s[i]);
 	return (0);
 }
