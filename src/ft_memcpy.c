f92f3d0811c7c16b38018f66f916e4491b1d5f09
gruz <mbistami@e2r12p7.1337.ma>
2021-11-11 01:16:18 +0100

    fix memcpy norm

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbistami <mbistami@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mbistami <mbistami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:01:44 by mbistami          #+#    #+#             */
-/*   Updated: 2021/11/09 18:02:13 by                  ###   ########.fr       */
+/*   Updated: 2021/11/11 01:16:10 by mbistami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	*ft_memcpy(void *dest, const char *src, size_t len)
 
 	d = dest;
 	s = src;
-    if(dest == NULL && src == NULL)
-        return (NULL);
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	while (len--)
 		*d++ = *s++;
 	return (dest);
